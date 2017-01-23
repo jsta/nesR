aa5d3bd85fffef3a2a6d0d5ccc803909473ca5be
jsta <jstachelek@utexas.edu>
2017-01-23 17:16:04 -0500

    print col headers if first page

--- vignettes/scrape_nes.R
+++ vignettes/scrape_nes.R
@@ -6,4 +6,11 @@ nes_file <- nes_file <- system.file("extdata/national-eutrophication-survey_1975
                                     package = "nesR")
 
 res <- nes_get(nes_file, as.numeric(args))
-write.table(res, file = "res.csv", append = TRUE, row.names = FALSE, col.names = FALSE, sep = ",")
+
+if(as.numeric(args) == 11){
+  write.table(res, file = "res.csv", append = TRUE, row.names = FALSE, col.names = TRUE, sep = ",")
+}else{
+  write.table(res, file = "res.csv", append = TRUE, row.names = FALSE, col.names = FALSE, sep = ",")
+}
+
+
