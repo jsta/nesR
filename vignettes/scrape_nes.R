de0e64f95e6254dd86ef72d8bfaaa788ee31f36e
jsta <jstachelek@utexas.edu>
2017-01-23 17:11:36 -0500

    better file writing

--- vignettes/scrape_nes.R
+++ vignettes/scrape_nes.R
@@ -6,4 +6,4 @@ nes_file <- nes_file <- system.file("extdata/national-eutrophication-survey_1975
                                     package = "nesR")
 
 res <- nes_get(nes_file, as.numeric(args))
-write(res, file = "res.csv", append = TRUE)
+write.table(res, file = "res.csv", append = TRUE, row.names = FALSE, col.names = FALSE, sep = ",")
