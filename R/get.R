991befd87f1e10610550a18fb9a935383e4bf0e3
jsta <jstachelek@utexas.edu>
2017-01-26 10:11:43 -0500

    delete image after ocr, closes #1

--- R/get.R
+++ R/get.R
@@ -6,13 +6,14 @@
 #' @examples \dontrun{
 #' nes_file <- system.file("extdata/national-eutrophication-survey_477.PDF",
 #'                      package = "nesR")
-#' nes_page <- 11
+#' nes_page <- 12
 #' nes_get(nes_file, nes_page)
 #' }
 
 nes_get <- function(nes_file, nes_page){
   tif_clean <- extract_nes_page(nes_file, nes_page)
   raw_txt   <- ocr_nes_page(tif_clean)
+  unlink(tif_clean)
   parse_nes(raw_txt)
 }
 
