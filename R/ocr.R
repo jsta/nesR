d9f2f927acffb4a9d823a631009758818eaedc18
jsta <stachel2@msu.edu>
2018-03-19 17:11:05 -0400

    enable blacklist

--- R/ocr.R
+++ R/ocr.R
@@ -1,7 +1,7 @@
 #' ocr_nes_page
 #' @param tif_clean file.path to tif image for ocr
 #' @export
-#' @importFrom tesseract ocr
+#' @importFrom tesseract ocr tesseract
 #' @examples \dontrun{
 #' nes_file <- system.file("extdata/national-eutrophication-survey_477.PDF",
 #'                      package = "nesR")
@@ -10,7 +10,7 @@
 #' ocr_nes_page(tif_clean)
 #' }
 ocr_nes_page <- function(tif_clean){
-	bad_char_blacklist <- tesseract(options = list(tessedit_char_blacklist = "!;¢"))
-  ocr_txt <- tesseract::ocr(tif_clean)
+	bad_char_blacklist <- tesseract(options = list(tessedit_char_blacklist = "!;¢?-"))
+  ocr_txt <- tesseract::ocr(tif_clean, engine = bad_char_blacklist)
   strsplit(ocr_txt, "\n")[[1]]
 }
